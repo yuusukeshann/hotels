1cf8104137fe8a5ae57df4c68a1a72a8a07162b2
yuusukeshann <ihate-gomikusokasuugliness@ezweb.ne.jp>
2023-01-11 22:32:37 +0900

    テスト記述の準備

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
 
 has_many :resavations
 
 validates :nickname, presence: true
+
+end
