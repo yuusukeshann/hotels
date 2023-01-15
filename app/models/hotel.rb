9fb3aa54bc6601d54aa48eba81e650d27cf05def
yuusukeshann <ihate-gomikusokasuugliness@ezweb.ne.jp>
2023-01-15 19:12:14 +0900

    Active hashの導入

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,7 +1,11 @@
 class Hotel < ApplicationRecord
 
-  has_many :rooms
-
   validates :name,                 presence: true
   validates :prefecture_id,        presence: true,  numericality: { other_than: 1, message: "can't be blank" }
+
+  
+  has_many :rooms
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
 end
