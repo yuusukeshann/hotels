3e99cd6df34a8f876800a7b03dc7d31250bae03f
yuusukeshann <ihate-gomikusokasuugliness@ezweb.ne.jp>
2023-02-05 14:49:53 +0900

    roomモデルバリデーション、アソシエーション

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,12 @@
 class Room < ApplicationRecord
+  validates :room_name,              presence: true
+  validates :room_size,              presence: true
+  validates :max_people_available,   presence: true
+  validates :guest_id,               presence: true,  numericality: { other_than: 1, message: "can't be blank" }
+
+  has_many :plans
+  belongs_to :hotel
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :guest
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'hotels/index'
   devise_for :users
   root to: "hotels#index"
-  resources :plans, only: []
+  resources :plans
 end
