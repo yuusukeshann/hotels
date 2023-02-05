70202135705cc75776067ee3f67ff809c5e6032a
yuusukeshann <ihate-gomikusokasuugliness@ezweb.ne.jp>
2023-02-05 14:51:13 +0900

    guestモデルの作成、アソシエーション

---
+++ app/models/guest.rb
@@ -0,0 +1,13 @@
+class Guest < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '１人' },
+    { id: 3, name: '２人' },
+    { id: 4, name: '３人' },
+    { id: 5, name: '４人' },
+    { id: 6, name: '５人' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :rooms
+  end
